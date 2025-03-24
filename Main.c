6cd2832cbcfada611094a23935650a6586fdef3e
Vhon Michael Tabalan <153250288+Xhyther@users.noreply.github.com>
2025-03-25 00:39:52 +0800

    added the Token Types

--- Main.c
+++ Main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 
+enum TokenType 
+{
+    Token_IDENTIFIER,
+    Token_LET,
+    Token_IF,
+    Token_ELSE,
+    Token_DO,
+    Token_WHILE,
+    Token_FOR,
+    Token_PRINT,
+    Token_ARITHMETI_OPERATIONS,
+    Token_CONDITIONS
+};
+
 int main(void)
 {
     printf("Hello World!");
