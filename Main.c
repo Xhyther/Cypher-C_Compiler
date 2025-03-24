c862896007d9a753d86a28dc9a80a60001a1b7af
Vhon Michael Tabalan <153250288+Xhyther@users.noreply.github.com>
2025-03-25 06:46:28 +0800

    Updated the token type list

--- Main.c
+++ Main.c
@@ -3,16 +3,41 @@
 
 enum TokenType 
 {
+    //Single-Character Tokens
+    Token_LEFT_PARENT,
+    Token_RIGHT_PARENT,
+    Token_LEFT_BRACE,
+    Token_RIGHT_BRACE,
+    Token_COMMA,
+    Token_DOT,
+    Token_MINUS,
+    Token_PLUS,
+    Token_SLASH,
+    Token_START,
+
+    //One or Two-Character Tokens
+    Token_BANG,
+    Token_BANG_EQUALS,
+    Token_ASSIGNMENT,
+    Token_EQUALITY,
+    Token_GREATER,
+    Token_GREATER_EQUAL,
+    Token_LESS,
+    Token_LESS_EQUAL,
+    
+    //Literals
     Token_IDENTIFIER,
+    Token_STRING,
+    Token_Number,
+
+    //Keywords
     Token_LET,
     Token_IF,
     Token_ELSE,
     Token_DO,
     Token_WHILE,
     Token_FOR,
-    Token_PRINT,
-    Token_ARITHMETI_OPERATIONS,
-    Token_CONDITIONS
+    Token_PRINT
 };
 
 int main(void)
