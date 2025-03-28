30862a83fe8314e3eda7f56efab9ca7dadbdfef6
Vhon Michael Tabalan <153250288+Xhyther@users.noreply.github.com>
2025-03-28 23:07:27 +0800

    error handling

--- Parser.h
+++ Parser.h
@@ -3,8 +3,43 @@
 #include "Tokens.h"
 
 typedef struct {
-    Token current;
-    Token previous;
+    Token currentToken;
+    Token prevToken;
+    bool hasError;
+    bool panicFlag;
+    const char *errorMessage;
 }Parser;
 
 Parser parser;
+
+static void error(const char* message){
+    errorAt(&parser.prevToken, message);
+}
+
+static void errorAtCurrent(const char* message){
+    errorAt(&parser.currentToken, message);
+}
+
+static void errorAt(Token* token, const char* message){
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == Token_EOF) fprintf(stderr, " at end");
+    else if (token->type == Token_ERROR) {} //Basiclly Ignore
+    else fprintf(stderr, " at '%.*s'", token->length, token->start);
+
+  fprintf(stderr, ": %s\n", message);
+  parser.hasError = true;
+}
+
+static void advanceParser(){
+    parser.prevToken = parser.currentToken;
+
+    for (;;)
+    {
+        parser.currentToken = scanToken();
+        if (parser.currentToken.type != Token_ERROR) break;
+        
+    }
+    
+}
+
